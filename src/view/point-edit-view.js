7c0f6cb69ccbc7c56275934ad9274cf5f8586cbf
Ocupuc123 <ocupuc123@mail.ru>
2026-07-27 09:50:34 +0300

    8.10. Дополнительная функциональность

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -119,7 +119,7 @@ const createPointEditTemplate = ({point, isNewPoint, cities}) => {
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">${isSaving ? 'Saving...' : 'Save'}</button>
-        ${isNewPoint ? '<button class="event__reset-btn" type="reset"}>Cancel</button>' : `<button class="event__reset-btn" type="reset">${isDeleting ? 'Deleting...' : 'Delete'}</button>`}
+        ${isNewPoint ? '<button class="event__reset-btn" type="reset">Cancel</button>' : `<button class="event__reset-btn" type="reset">${isDeleting ? 'Deleting...' : 'Delete'}</button>`}
         ${isNewPoint ? '' : `<button class="event__rollup-btn" type="button">
           <span class="visually-hidden">Open event</span>
         </button>`}
